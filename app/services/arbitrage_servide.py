189691dd714782ad3ead73f7db6e02c363f1507d
gabrielbaute <gabrielbaute@gmail.com>
2026-07-20 20:33:13 -0400

    Configuramos el servicio para expresar las tasas siempre en bs

--- app/services/arbitrage_servide.py
+++ app/services/arbitrage_servide.py
@@ -17,7 +17,12 @@ class ArbitrageService:
         self.binance_service = BinanceService(databasesession=databasesession)
         self.fiat_exchange_service = FiatExchangeService(databasesession=databasesession)
 
-    def _calculate_adquisition_rate(self, rate_1: BinanceCurrencyResponse, rate_2: BinanceCurrencyResponse) -> float:
+    def _calculate_adquisition_rate(
+            self, 
+            rate_1: BinanceCurrencyResponse, 
+            rate_2: BinanceCurrencyResponse, 
+            ignore_buy_flag: bool = True
+        ) -> float:
         """
         Calcula la tasa de adquisición de los activos fiat en relación al USDT de Binance.
 
@@ -31,21 +36,34 @@ class ArbitrageService:
         Raises:
             ValueError: Si el denominador no es BUY o su rate es 0.
         """
-        if rate_2.trade_type != TradeType.BUY:
-            raise ValueError("La tasa del denominador debe ser siempre la tasa de compra (BUY).")
         if rate_2.average_price == 0:
             raise ValueError("La tasa del denominador no puede ser cero (división por cero).")
-
+        
+        if ignore_buy_flag:
+            return rate_1.average_price / rate_2.average_price
+        
+        if rate_2.trade_type != TradeType.BUY:
+            raise ValueError("La tasa del denominador debe ser siempre la tasa de compra (BUY).")
+        
         return rate_1.average_price / rate_2.average_price
     
-    def _calculate_remesas_customer_price(self, adquisition_rate: float, reveneu_rate: float = 1.08) -> float:
+    def _calculate_remesas_customer_price(
+            self, 
+            adquisition_rate: float, 
+            reveneu_rate: float = 1.08, 
+            divide: bool = True
+        ) -> float:
         """
         Calcula el precio final que el prestador de servicios de remesas ofrecerá a sus clientes.
 
         Args:
             adquisition_rate (float): Tasa de adquisición del vendedor
             reveneu_rate (float): Tasa de ganancia del vendedor (default: 1.08, es decir 8%)
+            divide (bool): Si la dirección de operación require división, se indica aquí.
         """
+        if divide:
+            return adquisition_rate / reveneu_rate
+        
         return adquisition_rate * reveneu_rate
 
     async def get_today_remesas_price(self, reveneu_rate: float = 1.08) -> ArbitrageResponse:
@@ -61,8 +79,8 @@ class ArbitrageService:
         fiat_pair: FiatPairResponse = await self.fiat_exchange_service.get_pair(fiat_1=FiatCurrency.VES, fiat_2=FiatCurrency.PEN)
         
         ven_to_peru_adquisition_rate = self._calculate_adquisition_rate(
-            rate_1=fiat_pair.fiat_2_p2p_sell,
-            rate_2=fiat_pair.fiat_1_p2p_buy
+            rate_1=fiat_pair.fiat_1_p2p_buy,
+            rate_2=fiat_pair.fiat_2_p2p_sell
         )
         peru_to_ven_adquisition_rate = self._calculate_adquisition_rate(
             rate_1=fiat_pair.fiat_1_p2p_sell,
@@ -71,11 +89,13 @@ class ArbitrageService:
 
         ven_to_peru_customer_price = self._calculate_remesas_customer_price(
             adquisition_rate=ven_to_peru_adquisition_rate,
-            reveneu_rate=reveneu_rate
+            reveneu_rate=reveneu_rate,
+            divide=False
         )
         peru_to_ven_customer_price = self._calculate_remesas_customer_price(
             adquisition_rate=peru_to_ven_adquisition_rate,
-            reveneu_rate=reveneu_rate
+            reveneu_rate=reveneu_rate,
+            divide=True
         )
 
         return ArbitrageResponse(
